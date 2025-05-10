99dc7e12cbf0f8dac97fea43cfc15f6291a35a7a
Rafael Reis <rafaelrg0705@gmail.com>
2025-05-10 18:33:16 -0300

    fase final da estrutura

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//função de abrir o menu mobile
 document.addEventListener('DOMContentLoaded', ()=>{
     const botaoMenu = document.getElementById('botaoMenu').addEventListener('click',()=>{
         var menu = document.querySelector('.celular')
@@ -10,3 +12,33 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
     })
 })
+
+    //função para aparecer e sumir o texto do banner quando for clicado
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    let txtB1 = document.querySelector('.theBegginingBanner').addEventListener('click', ()=>{
+        verificarClasses('theBegginingBanner', 'p1', 'pView', 'bannerCanimation')
+    })
+
+    let txtB2 = document.querySelector('.darkKnightBanner').addEventListener('click', ()=>{
+        verificarClasses('darkKnightBanner', 'p2', 'pView', 'bannerCanimation')
+    })
+
+    let txtB3 = document.querySelector('.theBatmanBanner').addEventListener('click', ()=>{
+        verificarClasses('theBatmanBanner', 'p3', 'pView', 'bannerCanimation')
+    })
+})
+
+function verificarClasses (banner, pN, classAdRem, classBCA){
+    let ban = document.querySelector(`.${banner}`)
+    let p   = document.querySelector(`.${pN}`)
+
+    if(p.classList.contains(`${classAdRem}`)){
+        p.classList.remove(`${classAdRem}`)
+        ban.classList.add(`${classBCA}`)
+    }else{
+        p.classList.add(`${classAdRem}`)
+        ban.classList.remove(`${classBCA}`)
+    }
+}
+
